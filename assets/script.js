868c6b75d019ed4087df24b03ca40b4a55ac4d8d
SheikhRashed <devrashed06@gmail.com>
2022-03-20 22:48:55 +0600

    update: leaflet map added

--- assets/script.js
+++ assets/script.js
@@ -24,3 +24,31 @@ const months = [
   'November',
   'December',
 ];
+
+if (navigator.geolocation) {
+  navigator.geolocation.getCurrentPosition(
+    function (position) {
+      const { latitude, longitude } = position.coords;
+      console.log(position.coords);
+      // console.log(latitude, longitude);
+      // console.log(`https://www.google.com/maps/@${latitude},${longitude},15z`);
+
+      const coords = [latitude, longitude];
+      console.log(coords);
+      var map = L.map('map').setView(coords, 16);
+
+      L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        attribution:
+          '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+      }).addTo(map);
+
+      L.marker(coords)
+        .addTo(map)
+        .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
+        .openPopup();
+    },
+    function () {
+      console.log('Could not get your position');
+    }
+  );
+}
